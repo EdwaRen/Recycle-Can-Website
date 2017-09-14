66d5fab96b5c48eca7046f1a953b2873af5f9ed1
Edward Ren <eddie.ren.2013@gmail.com>
2017-09-13 22:25:18 -0400

    clean react state

--- AutoSelectJS copy/id.js
+++
@@ -1,6 +0,0 @@
-export function getIDs() {
-
-	//The selector id of the five selector on this particular webpage
-    var categories = ["lsd", "sec", "twp", "rng", "mrd"];
-    return categories;
-}
